1582fefd165a992fdc620e3dd2ff54c81026bf18
james <james.whiteman@gmail.com>
2018-05-04 10:21:42 +1200

    fabric reqs

---
+++ fabricReq.sh
@@ -0,0 +1,6 @@
+mkdir ~/fabric-dev-servers && cd ~/fabric-dev-servers
+
+curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.tar.gz
+tar -xvf fabric-dev-servers.tar.gz
+cd ~/fabric-dev-servers
+./downloadFabric.sh
